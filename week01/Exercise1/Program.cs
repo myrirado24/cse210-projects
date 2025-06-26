b884617dfe3f2ed57e5378141dbf898b7af44319
myrirado24 <gui18002@byui.edu>
2025-06-25 20:15:32 -0600

    Added a line

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("This is in C#.");
     }
 }
