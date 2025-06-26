4fc35d92e3d603c13495093e40c8a92b86abc9a3
myrirado24 <gui18002@byui.edu>
2025-06-25 21:17:03 -0600

    Completed week 1 Ex1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,6 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-        Console.WriteLine("This is in C#.");
+        Console.WriteLine("There will not be a newline after this");
+
+        Console.Write("What is yout favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your color is " + color);
+
+        // Start of assignment
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
