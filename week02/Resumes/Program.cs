0dbe8e8c896bd94da0cde43c5988042e5c28783c
myrirado24 <gui18002@byui.edu>
2025-07-03 22:00:57 -0600

    Added the start and end year

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -9,16 +9,18 @@ class Program
 
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
 
-        Console.WriteLine($"Job 1 Title: {job1._jobTitle}");
-        Console.WriteLine($"Job 1 Company: {job1._company}");
+        job1.Display();
 
         Job job2 = new Job();
 
         job2._jobTitle = "Manager";
         job2._company = "Apple";
+        job2._startYear = "2022";
+        job2._endYear = "2023";
 
-        Console.WriteLine($"Job 1 Title: {job2._jobTitle}");
-        Console.WriteLine($"Job 1 Company: {job2._company}");
+        job2.Display();
     }
 }
