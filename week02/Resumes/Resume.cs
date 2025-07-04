681c74d1e7311b576f497a23e7db516eba469650
myrirado24 <gui18002@byui.edu>
2025-07-03 22:26:04 -0600

    Added the display of my name and jobs

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -1,7 +1,20 @@
 using System;
+using System.IO.Compression;
+using System.Reflection.Emit;
 
 public class Resume
 {
-    public string _florencia;
+    public string _name;
     public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs: ");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
 }
